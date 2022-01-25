7381b7653c204414b473e5c4833875412b83fbf8
Geoffrey Sessums <gsessums@erpinternational.com>
2022-01-25 17:21:31 -0600

    Add the colors.js module, created Bootcamp model, added database connection, and updated createBootcamp.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
@@ -15,9 +17,26 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Create bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-}
+exports.createBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json ({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+};
+// exports.createBootcamp = (req, res, next) => {
+//     console.log(req.body);
+
+//     res.status(200).json ({
+//         success: true,
+//         msg: 'Create new bootcamp'
+//     });
+// }
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamp/:id
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const res = require('express/lib/response');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Custom logger example
@@ -17,6 +18,9 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // For custom logger example
 // app.use(logger);
@@ -34,13 +38,13 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
     )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     // Close server & exit process
     server.close(() => process.exit(1));
 })
