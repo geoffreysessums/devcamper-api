9e55ea53448929615dcab704b5464a8911022d29
Geoffrey Sessums <gsessums@erpinternational.com>
2022-02-05 13:47:39 -0600

    Added a method to add a course to a specific bootcamp

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get courses
 // @route   GET /api/v1/courses
@@ -42,6 +43,29 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
             404
         );
     }
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+// @desc    Add course
+// @route   PUT /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`No course with the id of ${req.params.bootcampId}`),
+            404
+        );
+    }
+
+    const course = await Course.create(req.body);
+
     res.status(200).json({
         success: true,
         data: course
--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const {
     getCourses,
-    getCourse 
+    getCourse,
+    addCourse 
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
 router.route('/:id').get(getCourse);
 
 module.exports = router;
