b95a84fd606128436bb217038978e4ba470746e4
Geoffrey Sessums <gsessums@erpinternational.com>
2022-02-07 06:26:58 -0600

    Added a method to update a course

--- routes/courses.js
+++ routes/courses.js
@@ -2,12 +2,13 @@ const express = require('express');
 const {
     getCourses,
     getCourse,
-    addCourse 
+    addCourse,
+    updateCourse 
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse);
 
 module.exports = router;
