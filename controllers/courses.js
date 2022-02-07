ab1718cb43f43d7155f9794777a0888e577273e5
Geoffrey Sessums <gsessums@erpinternational.com>
2022-02-07 08:35:36 -0600

    Added a method to delete a single course

--- controllers/courses.js
+++ controllers/courses.js
@@ -95,3 +95,24 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
         data: course
     });
 });
+
+// @desc    delete course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(
+            new ErrorResponse(`No course with the id of ${req.params.id}`),
+            404
+        );
+    }
+
+    await Course.deleteOne(course);
+
+    res.status(200).json({
+        success: true,
+        data: {} 
+    });
+});
--- routes/courses.js
+++ routes/courses.js
@@ -3,12 +3,13 @@ const {
     getCourses,
     getCourse,
     addCourse,
-    updateCourse 
+    updateCourse,
+    deleteCourse 
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
