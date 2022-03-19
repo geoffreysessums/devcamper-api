98e726e109451cb0aec8cc54d8844199b2325225
Geoffrey Sessums <gsessums@erpinternational.com>
2022-03-19 10:25:16 -0500

    Refactored results filtering into advancedResults middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,7 +9,10 @@ const {
     bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
-// Include other resourse routers
+// Include other resource routers
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 const courseRouter = require('./courses');
 
 const router = express.Router();
@@ -21,7 +24,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
